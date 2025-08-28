6cac8e60858fc1e03b39362e622783fb3e2cdc50
tr1fker <cfjhxhkxck@gmail.com>
2025-08-28 21:43:21 +0300

    Added task model

---
+++ src/main/java/tr1fker/models/Task.java
@@ -0,0 +1,59 @@
+package tr1fker.models;
+
+public class Task {
+    private int id;
+    private String name;
+    private Priority priority;
+    private String description;
+    private Status status;
+
+    public Task(int id, String name, Priority priority, String description, Status status) {
+        this.id = id;
+        this.name = name;
+        this.priority = priority;
+        this.description = description;
+        this.status = status;
+    }
+    
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Priority getPriority() {
+        return this.priority;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
