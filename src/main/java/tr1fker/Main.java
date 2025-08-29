c9d7ccb0a1357ebe4229527b16fe301e48ac2298
tr1fker <cfjhxhkxck@gmail.com>
2025-08-29 23:01:16 +0300

    Runned functional custom main

--- src/main/java/tr1fker/Main.java
+++ src/main/java/tr1fker/Main.java
@@ -1,6 +1,13 @@
 package tr1fker;
 
+import tr1fker.handlers.InputHandler;
+import tr1fker.handlers.UIHandler;
+
 public class Main {
     public static void main(String[] args) {
+        InputHandler inputHandler = new InputHandler();
+        UIHandler uIHandler = new UIHandler(inputHandler);
+
+        uIHandler.startConsole();
     }
 }
