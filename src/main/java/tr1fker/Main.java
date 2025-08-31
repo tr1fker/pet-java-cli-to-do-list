a86c28f44ba3238a21f8d12b87b83aec50447431
tr1fker <cfjhxhkxck@gmail.com>
2025-08-31 17:54:48 +0300

    The logic has been slightly changed

--- src/main/java/tr1fker/Main.java
+++ src/main/java/tr1fker/Main.java
@@ -2,11 +2,18 @@ package tr1fker;
 
 import tr1fker.handlers.InputHandler;
 import tr1fker.handlers.UIHandler;
+import tr1fker.managers.TaskManager;
+import tr1fker.views.OutputConsole;
 
 public class Main {
     public static void main(String[] args) {
-        InputHandler inputHandler = new InputHandler();
-        UIHandler uIHandler = new UIHandler(inputHandler);
+        OutputConsole outputConsole = new OutputConsole();
+
+        InputHandler inputHandler = new InputHandler(outputConsole);
+
+        TaskManager taskManager = new TaskManager();
+
+        UIHandler uIHandler = new UIHandler(inputHandler, taskManager, outputConsole);
 
         uIHandler.startConsole();
     }
