543fe035c9a1d9a9d8b74aa354d55646764eed52
tr1fker <cfjhxhkxck@gmail.com>
2025-08-31 17:52:56 +0300

    Added method to create a task

--- src/main/java/tr1fker/handlers/UIHandler.java
+++ src/main/java/tr1fker/handlers/UIHandler.java
@@ -1,18 +1,26 @@
 package tr1fker.handlers;
 
+import tr1fker.managers.TaskManager;
+import tr1fker.models.Priority;
+import tr1fker.models.Status;
+import tr1fker.models.Task;
 import tr1fker.views.OutputConsole;
 
 public class UIHandler {
     private InputHandler inputHandler;
 
+    private TaskManager taskManager;
+
     private OutputConsole outputConsole;
 
     private boolean isRunning;
 
-    public UIHandler(InputHandler inputHandler){
+    public UIHandler(InputHandler inputHandler, TaskManager taskManager, OutputConsole outputConsole){
         this.inputHandler = inputHandler;
 
-        this.outputConsole = new OutputConsole();
+        this.taskManager = taskManager;
+
+        this.outputConsole = outputConsole;
 
         this.isRunning = false;
     }
@@ -29,6 +37,7 @@ public class UIHandler {
 
             switch(option){
                 case 1:
+                    this.createTask();
                     break;
                 case 2:
                     break;
@@ -43,6 +52,22 @@ public class UIHandler {
                     break;
             }
         }
+    }
+
+    public void createTask(){
+        this.outputConsole.printINameTask();
+        String name = this.inputHandler.inputString();
+
+        this.outputConsole.printPriorities(Priority.values());
+        this.outputConsole.printINumber();
+        Priority priority = this.inputHandler.inputPriority();
+
+        this.outputConsole.printIDescTask();
+        String description = this.inputHandler.inputString();
+
+        Task newTask = new Task(name, priority, description, Status.NEW);
+
+        this.taskManager.add(newTask);
     }
 
     public void stopConsole(){
