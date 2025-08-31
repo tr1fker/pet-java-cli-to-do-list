c787c91e467307df9774dea70c15d4f827538526
tr1fker <cfjhxhkxck@gmail.com>
2025-08-31 17:50:25 +0300

    Added method inputPriority and added in inputInteger reading '\n'

--- src/main/java/tr1fker/handlers/InputHandler.java
+++ src/main/java/tr1fker/handlers/InputHandler.java
@@ -1,19 +1,38 @@
 package tr1fker.handlers;
 
+import tr1fker.models.Priority;
+import tr1fker.views.OutputConsole;
+
 import java.util.Scanner;
 
 public class InputHandler {
     private static final Scanner scanner;
 
+    protected OutputConsole outputConsole;
+
     static{
         scanner = new Scanner(System.in);
     }
 
+    public InputHandler(OutputConsole outputConsole){
+        this.outputConsole = outputConsole;
+    }
+
     public int inputInteger(){
-        return scanner.nextInt();
+        int number = scanner.nextInt();
+        scanner.nextLine(); // '\n' считываем
+        return number;
     }
 
     public String inputString(){
-        return scanner.next();  
+        return scanner.nextLine();
+    }
+
+    public Priority inputPriority(){
+        Priority[] priorities = Priority.values();
+
+        int choice = this.inputInteger();
+
+        return priorities[choice];
     }
 }
